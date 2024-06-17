85888a886537770427fb2c707edbae0474f4fef9
Marcos Colella <colella.dev@gmail.com>
2024-06-17 11:58:00 -0300

    Componente Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
 
@@ -34,6 +35,11 @@ export function Groups() {
         ListEmptyComponent={() => (  
           <ListEmpty message="Que tal cadastrar a primeira turma?"/>
         )}
+      />
+
+      <Button 
+        title='Criar nova turma'
+       
       />
 
     </Container>
