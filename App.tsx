3a2924540fa14c3837603c7c99dce26d271232c7
Marcos Colella <colella.dev@gmail.com>
2024-07-02 08:58:23 -0300

    Criando Interface Players

--- App.tsx
+++ App.tsx
@@ -4,8 +4,8 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import theme from './src/theme';
 
-import { Groups } from '@screens/Groups';
 import { Loading } from '@components/Loading';
+import { Players } from '@screens/Players';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +17,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-      { fontsLoaded ? <Groups/> : <Loading/> }
+      { fontsLoaded ? <Players/> : <Loading/> }
     </ThemeProvider>
   );
 }
