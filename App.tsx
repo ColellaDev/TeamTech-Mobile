7702fa2a4c0650bc42ecf19ce0c0143cce88646c
Marcos Colella <colella.dev@gmail.com>
2024-07-03 17:35:45 -0300

    Estrutura de navegação

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import theme from './src/theme';
 
 import { Loading } from '@components/Loading';
-import { Players } from '@screens/Players';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +17,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-      { fontsLoaded ? <Players/> : <Loading/> }
+      { fontsLoaded ? <Routes/> : <Loading/> }
     </ThemeProvider>
   );
 }
